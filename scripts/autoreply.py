e3a4ed7a4ae3fc4198b1cbb2def5679b26ff4ab3
Jonathan de Steuben <jonathandesteuben@Jonathans-MacBook-Air.local>
2021-07-07 10:53:18 -0400

    Changed the keyword variable for this test. Need to make corrections to the replies still, and add better logging for this script in the project folder next. Back to the drawing board.

--- scripts/autoreply.py
+++ scripts/autoreply.py
@@ -8,11 +8,12 @@ import time
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
+
 def check_mentions(api, keywords, since_id):
     logger.info("Retrieving mentions...")
     new_since_id = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline,
-        since_id=since_id).items():
+                               since_id=since_id).items():
         new_since_id = max(tweet.id, new_since_id)
         if tweet.in_reply_to_status_id is not None:
             continue
@@ -23,18 +24,22 @@ def check_mentions(api, keywords, since_id):
                 tweet.user.follow()
 
             api.update_status(
-                status="This is an automation test - DM for info if needed.",
-                in_reply_to_status_id=tweet.id,
+                status=f"Hi {tweet.user.name}. This is an automated response.",
+                in_reply_to_status_id=tweet.id
             )
     return new_since_id
+
 
 def main():
     api = create_api()
     since_id = 1
     while True:
-        since_id = check_mentions(api, ["test", "help"], since_id)
+        since_id = check_mentions(api, ["hello", "hi", "hey", "sup?"], since_id)
         logger.info("Waiting...")
-        time.sleep(120)
+        time.sleep(60)
 
-if __name__ == "__main__":
+
+if __name__ != "__main__":
+    pass
+else:
     main()
